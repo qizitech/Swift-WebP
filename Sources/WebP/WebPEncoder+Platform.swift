70906cd47171171c96c6fdd2e6869af1c80c1d72
JayZhao <zhaojianyin@icloud.com>
2019-08-01 18:14:44 +0800

    Fix image orientation

--- Sources/WebP/WebPEncoder+Platform.swift
+++ Sources/WebP/WebPEncoder+Platform.swift
@@ -41,12 +41,64 @@ import Foundation
         }
         
         private func convertUIImageToCGImageWithRGBA(_ image: UIImage) -> CGImage {
+            
+            //Fix orientation if needed
+            var transform: CGAffineTransform = .identity
+            let imageOrientation = image.imageOrientation
+            let size = image.size
+            
+            switch imageOrientation {
+            case .down, .downMirrored:
+                transform = transform.translatedBy(x: size.width, y: size.height)
+                transform = transform.rotated(by:.pi)
+                break
+            case .left, .leftMirrored:
+                transform = transform.translatedBy(x: size.width, y: 0)
+                transform = transform.rotated(by:.pi/2)
+                break
+            case .right, .rightMirrored:
+                transform = transform.translatedBy(x: 0, y: size.height)
+                transform = transform.rotated(by:-.pi/2)
+                break
+            case .up, .upMirrored:
+                break
+            @unknown default:
+                break
+            }
+            
+            switch imageOrientation {
+            case .upMirrored, .downMirrored:
+                transform.translatedBy(x: size.width, y: 0)
+                transform.scaledBy(x: -1, y: 1)
+                break
+            case .leftMirrored, .rightMirrored:
+                transform.translatedBy(x: size.height, y: 0)
+                transform.scaledBy(x: -1, y: 1)
+            case .up, .down, .left, .right:
+                break
+            @unknown default:
+                break
+            }
+            
             let colorSpace = CGColorSpaceCreateDeviceRGB()
             let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-            let context = CGContext(data: nil, width: Int(image.size.width), height: Int(image.size.height),
-                                    bitsPerComponent: 8, bytesPerRow: Int(image.size.width) * 4,
+            let context = CGContext(data: nil, width: Int(size.width), height: Int(size.height),
+                                    bitsPerComponent: 8, bytesPerRow: Int(size.width) * 4,
                                     space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
-            let rect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+            context.concatenate(transform)
+            
+            let rect:CGRect
+            switch imageOrientation {
+            case .left, .leftMirrored, .right, .rightMirrored:
+                rect = CGRect(x: 0, y: 0, width: size.height, height: size.width)
+                
+                break
+            default:
+                rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+                break
+            }
+            
+            
             context.draw(image.cgImage!, in: rect)
             
             return context.makeImage()!
